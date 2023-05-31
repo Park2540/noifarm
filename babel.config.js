4d1c60b2fed66c88def6a1aae2cab9ef0a49d07d
Park2540 <paklovethai.54@gmail.com>
2023-05-31 23:32:31 +0700

    first commit

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,13 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: [['module:metro-react-native-babel-preset'],
+            ['@babel/preset-env', '@babel/preset-react'],],
   plugins: [
     ['react-native-reanimated/plugin', {
       relativeSourceLocation: true,
     }],
     ['@babel/plugin-proposal-decorators', { legacy: true }],
     ['@babel/plugin-transform-runtime'],
-    '@babel/plugin-proposal-export-namespace-from',
+    ['@babel/plugin-proposal-export-namespace-from'],
+    
   ],
 };
