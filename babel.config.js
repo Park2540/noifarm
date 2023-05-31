93bed9e6d39b72b25b20aaa9dd1623d6d13424f1
Park2540 <paklovethai.54@gmail.com>
2023-05-31 23:46:04 +0700

    first commit

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,23 @@
 module.exports = {
-  presets: [['module:metro-react-native-babel-preset'],
-            ['@babel/preset-env', '@babel/preset-react'],],
+  presets: [
+    ["module:metro-react-native-babel-preset"],
+    ["@babel/preset-env", { "loose": false }],
+    ["@babel/preset-react"],
+    // ["module:metro-react-native-babel-preset"],
+    // ["@babel/preset-env", { "loose": false }],
+    // ["@babel/preset-react"]
+  ],
   plugins: [
-    ['react-native-reanimated/plugin', {
-      relativeSourceLocation: true,
-    }],
-    ['@babel/plugin-proposal-decorators', { legacy: true }],
-    ['@babel/plugin-transform-runtime'],
-    ['@babel/plugin-proposal-export-namespace-from'],
-    
+    // ['react-native-reanimated/plugin', {
+    //   relativeSourceLocation: true,
+    // }],
+    ["react-native-reanimated/plugin", { "relativeSourceLocation": true }],
+    ["@babel/plugin-transform-runtime"],
+    ["@babel/plugin-proposal-export-namespace-from"],
+    ["@babel/plugin-transform-class-properties", { "loose": true }],
+    ["@babel/plugin-transform-private-methods", { "loose": true }],
+    ["@babel/plugin-transform-private-property-in-object", { "loose": true }],
+    ["@babel/plugin-proposal-decorators", { "legacy": true }],
+    ["@babel/plugin-proposal-decorators", { "legacy": true }, "unique-name"]
   ],
 };
